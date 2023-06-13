052c249107799c036c08ab406e48b7e3773b75da
boyaveereshb <naruveeresh1999@gmail.com>
2023-06-13 11:30:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+import Form from './Form';
+
+import DataTable from './DataTable';
+
+
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+
+<Routes>
+
+  <Route path="/" element={<Form/>}></Route>
+
+  <Route path="/Datatable" element={<DataTable/>}></Route>
+
+</Routes >
+
+</BrowserRouter>
     </div>
   );
 }
